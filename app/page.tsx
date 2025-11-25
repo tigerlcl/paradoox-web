fe678c23abdc9f676c6001375fa15620b5b58ece
Tiger Li <tigerlicuhk@gmail.com>
2025-11-25 21:34:31 +0800

    redesign home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,26 @@
 'use client'
 
-import { useState } from 'react'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
-import { ProblemSection, SolutionSection, CustomerSection } from '@/components/Sections'
+import { 
+  WhyAIFailsSection, 
+  MissionSection, 
+  ResearchSection 
+} from '@/components/Sections'
 import Footer from '@/components/Footer'
-import AnimatedBackground from '@/components/AnimatedBackground'
-import WaitlistModal from '@/components/WaitlistModal'
 
 
 export default function Home() {
-  const [isWaitlistOpen, setIsWaitlistOpen] = useState(false)
-
   return (
-    <main className="min-h-screen relative">
-      <AnimatedBackground />
+    <main className="min-h-screen relative bg-black">
       <div className="relative z-10">
-        <Header onWaitlistOpen={() => setIsWaitlistOpen(true)} />
+        <Header />
         <Hero />
-        <ProblemSection />
-        <SolutionSection />
-        <CustomerSection />
+        <WhyAIFailsSection />
+        <ResearchSection />
+        <MissionSection />
         <Footer />
       </div>
-      
-      {/* Waitlist Modal - Rendered at page level for full coverage */}
-      <WaitlistModal 
-        isOpen={isWaitlistOpen} 
-        onClose={() => setIsWaitlistOpen(false)} 
-      />
     </main>
   )
 } 
